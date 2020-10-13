7d6444d27c0cd374976ed0ea1f407d8e90dcf1cd
Sugin Paladin <34408293+sugin223pl@users.noreply.github.com>
2020-10-14 02:50:54 +0300

    Added Electron

---
+++ dist_electron/bundled/precache-manifest.850227086428222d29b87183d2c199d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6267ea284a0ba5add6e",
+    "url": "app://./css/app.71ce79b5.css"
+  },
+  {
+    "revision": "1bec0a26162210388c98",
+    "url": "app://./css/chunk-vendors.53968962.css"
+  },
+  {
+    "revision": "3dcf7feac5cbb2f207fe4f6825ef4d73",
+    "url": "app://./img/gh.3dcf7fea.png"
+  },
+  {
+    "revision": "abee815cb908fe8f21cce5608eeb5510",
+    "url": "app://./img/winstorebanner-300x108.abee815c.png"
+  },
+  {
+    "revision": "fa46bc75268b95e0ff9de7d7a58b8ed4",
+    "url": "app://./index.html"
+  },
+  {
+    "revision": "a6267ea284a0ba5add6e",
+    "url": "app://./js/app.68de122a.js"
+  },
+  {
+    "revision": "1bec0a26162210388c98",
+    "url": "app://./js/chunk-vendors.61fc0558.js"
+  },
+  {
+    "revision": "a6aebf5951660833839dba078b4e8b3b",
+    "url": "app://./manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "app://./robots.txt"
+  }
+]);
